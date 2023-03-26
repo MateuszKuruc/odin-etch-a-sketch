91cbc39b8bd46daf5bdd10ed52b998c7a42b1734
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-03-26 21:31:10 +0200

    add basic logic, css styling and manipulate html
    add basic logic functions, css styling and manipulate html DOM

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
 const container = document.querySelector('#container');
+const sizeButton = document.querySelector('#sizeButton');
 
+function getSize() {
+    // let userChoice = 0;
+    let userInput = prompt('Choose between 2 and 99 for the panel width.');
+    if (userInput == null || userInput <= 1 || userInput > 99) {
+        userInput = prompt('Make sure the number is between 2 and 99!');
+        if (userInput == null || userInput <= 1 || userInput > 99) {
+            defaultSetting();
+        }   else if (userInput != null && userInput >= 2 || userInput <= 99) {
+            userChoice = Number(userInput);
+            return userChoice;
+        }
+    }   else if (userInput != null && userInput >= 2 || userInput <= 99) {
+        userChoice = Number(userInput);
+        return userChoice;
+    }   else {
+        defaultSetting();
+    }
+   
+}
+// user input and box generation - working
+sizeButton.addEventListener('click', changeSize);
+resetButton.addEventListener('click', resetGrid);
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-div.style.border = '1px solid black'
-div.style.margin = '0px';
-div.style.gap = '0px';
-div.style.minWidth = '50px';
-div.style.minHeight = '50px';
-container.appendChild(div)
+function changeSize() {
+    resetGrid();
+    makeGrid();
+}
 
+function resetGrid() {
+    container.replaceChildren();
+}
+
+function makeGrid(number) {
+    number = getSize();
+    for(let i = 0; i < number; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let k = 0; k < number; k++) {
+            let column = document.createElement('div');
+            column.classList.add('column')
+            row.appendChild(column);
+    }
 } 
-container.style.display = 'flex';
-container.style.flexWrap = 'wrap';
-container.style.width = '832px';
-container.style.height = '832px';
-container.style.margin = '0px';
-container.style.gap = '0px';
-container.style.border = '5px solid black';
+}
+
+
+function defaultSetting() {
+    let number = 16;
+    for (let i = 0; i < number; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let k = 0; k < number; k++) {
+            let column = document.createElement('div');
+            column.classList.add('column')
+            row.appendChild(column);
+    }
+}
+}
+
